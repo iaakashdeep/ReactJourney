6e6ead896ac9e9278c3ce69211efdbccb95d94f3
iaakashdeep <deep.0812@gmail.com>
2023-09-06 20:12:45 +0530

    First git commit

---
+++ App.js
@@ -0,0 +1,27 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", name: "abc" },
+  "Script Calling from React Code!" //Shows what we have to put inside this element
+);
+
+console.log(heading); //returns object
+//const root = ReactDOM.createRoot(document.getElementById("divStart"));
+
+//root.render(heading);
+
+//heading is an object when we print this in the console heading has so many attributes like type etc.  { id: "heading", name: "abc" } we provided in the heading object this shows as the attributes for this object.
+
+//Now when tis heading object is passed to the render method, this method will convert this object into browser understandable code and converts into html code to render.
+
+const parent = React.createElement("div", { id: "divInside" }, [
+  React.createElement("div", { id: "divChild" }, [
+    React.createElement("h1", {}, "I'm a h1 tag!"),
+    React.createElement("h2", {}, "I'm a h2 tag!"),
+  ]),
+  React.createElement("div", { id: "divChild2" }, [
+    React.createElement("h1", {}, "I'm a h1 tag!"),
+    React.createElement("h2", {}, "I'm a h2 tag!"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("divStart")); // createRoot is responsible to create a root in React
+root.render(parent);
